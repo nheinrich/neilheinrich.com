bfaa714985c0634b3bf3fac30adf017c9e20d24f
Neil Heinrich <nheinrich@gmail.com>
2025-08-17 17:16:04 -0600

    ✨ | add icons

---
+++ src/lib/components/Arrow.svelte
@@ -0,0 +1,27 @@
+<svg class="arrow" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+  <path
+    d="M11 0.55542L9.06125 2.49417L16.7337 10.1804H0V12.9304H16.7337L9.06125 20.6167L11 22.5554L22 11.5554L11 0.55542Z"
+  ></path>
+</svg>
+
+<style>
+  :root {
+    --arrow-width: 18px;
+  }
+  @media (min-width: 576px) {
+    :root {
+      --arrow-width: 20px;
+    }
+  }
+
+  @media (min-width: 992px) {
+    :root {
+      --arrow-width: 24px;
+    }
+  }
+
+  svg {
+    display: inline-block;
+    width: var(--arrow-width);
+  }
+</style>
