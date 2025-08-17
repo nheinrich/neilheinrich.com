1d899944e19803a4b7bd9558e3960cd21db29745
Neil Heinrich <nheinrich@gmail.com>
2025-08-17 17:54:25 -0600

    ✨ | update favicon implementation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
 	import '$lib/styles/reset.css';
 	import '$lib/styles/global.css';
 	import SlideInArticle from '$lib/components/SlideInArticle.svelte';
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" type="image/svg+xml" href={favicon} />
-</svelte:head>
 
 {@render children?.()}
 
